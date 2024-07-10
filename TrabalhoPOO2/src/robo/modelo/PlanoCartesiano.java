cc633afe7d98d978350171aadd0ba0309bf3ddb4
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-07-10 16:06:48 -0300

    commit plano

--- TrabalhoPOO2/src/robo/modelo/PlanoCartesiano.java
+++ TrabalhoPOO2/src/robo/modelo/PlanoCartesiano.java
@@ -1,6 +1,7 @@
 package robo.modelo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlanoCartesiano {
 	@SuppressWarnings("unused")
@@ -16,6 +17,59 @@ public class PlanoCartesiano {
 		this.foodY = foodY;
 		this.dimX = foodX;
 		this.dimY = foodY;
+	}
+	
+	
+	
+	public int getDimX() {
+		return dimX;
+	}
+
+
+
+	public void setDimX(int dimX) {
+		this.dimX = dimX;
+	}
+
+
+
+	public int getDimY() {
+		return dimY;
+	}
+
+
+
+	public void setDimY(int dimY) {
+		this.dimY = dimY;
+	}
+
+
+
+	public int getFoodX() {
+		return foodX;
+	}
+
+
+
+	public void setFoodX(int foodX) {
+		this.foodX = foodX;
+	}
+
+
+
+	public int getFoodY() {
+		return foodY;
+	}
+
+
+
+	public void setFoodY(int foodY) {
+		this.foodY = foodY;
+	}
+
+
+	public void adicionarRobos(Robo... robos) {
+		this.robos.addAll(Arrays.asList(robos));
 	}
 	
 	public void expandirX() {
