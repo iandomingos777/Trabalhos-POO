9fd6c885f288cdf7dfd3330e9d7e6e5ecafb03c3
Vitorfol <vitorfol2302@gmail.com>
2024-07-09 23:57:02 -0300

    Update teste.java

--- TrabalhoPOO2/src/testando/teste.java
+++ TrabalhoPOO2/src/testando/teste.java
@@ -1,7 +1,10 @@
 package testando;
 
 public class teste {
-private String nome;
-private int id;
-private String cpf;
+	private String nome;
+	private int id;
+	private String cpf;
+	public void cometerAtos() {
+		System.out.println("oi");
+	}
 }
