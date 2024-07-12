2a4a44bfcb7e4e094e8ae38d4da5106e1a31facb
Vitorfol <vitorfol2302@gmail.com>
2024-07-12 18:13:39 -0300

    Main2 - Criada

---
+++ TrabalhoPOO2/src/robo/Main2.java
@@ -0,0 +1,31 @@
+package robo;
+
+import java.util.Scanner;
+
+import robo.visao.PlanoCartesianoConsole;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		
+        Scanner entrada = new Scanner(System.in);
+        int x, y;
+        
+        do {
+            System.out.print("Posição X da comida: ");
+            x = entrada.nextInt();
+            System.out.print("Posição Y da comida: ");
+            y = entrada.nextInt();
+            System.out.println();
+        } while((x < 0 || y < 0) || (x == 0 && y == 0)) ;
+        
+		
+        PlanoCartesianoConsole plano = new PlanoCartesianoConsole(x, y); 
+        
+        plano.jogarMain2();
+        
+        entrada.close();
+		
+	}
+	
+}
