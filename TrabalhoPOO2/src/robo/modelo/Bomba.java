e1dc22a346a93b02fa1b86afbcdcd96651b299f5
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-07-15 16:25:06 -0300

    strings de bomba e rocha

--- TrabalhoPOO2/src/robo/modelo/Bomba.java
+++ TrabalhoPOO2/src/robo/modelo/Bomba.java
@@ -2,15 +2,18 @@ package robo.modelo;
 
 public class Bomba extends Obstaculo{
 
-	public Bomba(int posX, int posY) {
-		super(posX, posY);
-		// TODO Auto-generated constructor stub
-	}
-
 	@Override
-	public void bater() {
-		// TODO Auto-generated method stub
-		
+	public void bater(Robo robo) {
+	    robo.setPosX(-1);
+	    robo.setPosY(-1);
 	}
+	
+	 public String toString() {
+	        String bomba = "\uD83D\uDCA3";
+	        String blackColor = "\u001B[30m";
+	        String resetColor = "\u001B[0m";
+	        
+	        return blackColor + bomba + resetColor;
+	    }
 
 }
