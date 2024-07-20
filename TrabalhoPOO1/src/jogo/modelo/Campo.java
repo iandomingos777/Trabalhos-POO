4bdb40080067ac717be9be6d93c0e74a0253ae67
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-07-20 00:13:26 -0300

    commit square

--- TrabalhoPOO1/src/jogo/modelo/Campo.java
+++ TrabalhoPOO1/src/jogo/modelo/Campo.java
@@ -1,5 +1,45 @@
 package jogo.modelo;
 
+import java.util.ArrayList;
+
 public class Campo {
+	int number;
+private ArrayList<Jogador> players = new ArrayList<Jogador>();
+
+public void addPlayer(Jogador player) {
+	players.add(player);
+}
+
+public void remPlayer(Jogador player) {
+	for(int i = 0; i < players.size(); i++) {
+		if(players.get(i).getId() == player.getId()) {
+			players.remove(i);
+		}
+	}
+}
+
+public String toString() {
+	StringBuilder sb = new StringBuilder();
+	if((number >= 0 && number <=10)  || (number >= 31 && number <= 40)) {
+		sb.append("[");
+		for(int i = 0; i < players.size(); i++) {
+			sb.append(players.get(i));
+		}
+		for(int i = 0; i < 6 - players.size(); i++) {
+			sb.append(" ");
+		}
+		sb.append("]");
+	}else {
+		sb.append("_\n");
+		for(int i = 0; i < players.size(); i++) {
+			sb.append("|" + players.get(i) + "|\n");
+		}
+		for(int i = 0; i < 6 - players.size(); i++) {
+			sb.append("| |\n");
+		}
+		sb.append("_\n");
+	}
+	return sb.toString();
+}
 
 }
