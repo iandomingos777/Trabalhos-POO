51d586e4533b33c924a338a7f8865c60d4dae682
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-07-22 16:43:57 -0300

    commit impressao do tabuleiro

--- TrabalhoPOO1/src/jogo/modelo/Campo.java
+++ TrabalhoPOO1/src/jogo/modelo/Campo.java
@@ -4,30 +4,39 @@ import java.util.ArrayList;
 
 public class Campo {
 	int number;
-private ArrayList<Jogador> players = new ArrayList<Jogador>();
+	private ArrayList<Jogador> players = new ArrayList<Jogador>();
 
+	public ArrayList<Jogador> getPlayers() {
+		return players;
+	}
 
+	public Campo(int number) {
+		this.number = number;
+	}
 
-public ArrayList<Jogador> getPlayers() {
-	return players;
-}
+	public void addPlayer(Jogador player) {
+		players.add(player);
+	}
 
-
-public Campo(int number) {
-	this.number = number;
-}
-
-public void addPlayer(Jogador player) {
-	players.add(player);
-}
-
-public void remPlayer(Jogador player) {
-	for(int i = 0; i < players.size(); i++) {
-		if(players.get(i).getId() == player.getId()) {
-			players.remove(i);
+	public void remPlayer(Jogador player) {
+		for (int i = 0; i < players.size(); i++) {
+			if (players.get(i).getId() == player.getId()) {
+				players.remove(i);
+			}
 		}
 	}
-}
 
-
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		int length = players.size();
+		for (int i = 0; i < length; i++) {
+			sb.append(players.get(i));
+		}
+		for (int i = 0; i < 6 - length; i++) {
+			sb.append(" ");
+		}
+		sb.append("]");
+		return sb.toString();
+	}
 }
--- TrabalhoPOO1/src/jogo/modelo/Tabuleiro.java
+++ TrabalhoPOO1/src/jogo/modelo/Tabuleiro.java
@@ -10,20 +10,20 @@ public class Tabuleiro {
 	private ArrayList<Campo> squares = new ArrayList<>(41);
 
 	public Tabuleiro() {
-		for(int i = 0; i < 41; i++) {
+		for (int i = 0; i < 41; i++) {
 			squares.add(new Campo(i));
 		}
 	}
-	
+
 	public void moveInSquare(Jogador jogador) {
 
 		if (jogador.isBlocked()) {
 			jogador.setBlocked(false);
 			return;
 		}
-  
+
 		squares.get(jogador.getPosition()).remPlayer(jogador);
-		
+
 		Random random = new Random();
 		int dado1 = random.nextInt(6) + 1;
 		System.out.print("Dado 1: ");
@@ -32,11 +32,10 @@ public class Tabuleiro {
 		System.out.print("Dado 2: ");
 		System.out.println(dado2);
 		int sum = dado1 + dado2;
-		if(jogador instanceof JogadorAzarado && sum > 6) {
+		if (jogador instanceof JogadorAzarado && sum > 6) {
 			System.out.println("Jogador é azarado, soma dos dados não pode ser maior que 6");
 			sum = 5;
-		}
-		else if(jogador instanceof JogadorSortudo && sum < 7) {
+		} else if (jogador instanceof JogadorSortudo && sum < 7) {
 			System.out.println("Jogador é sortudo, a soma dos dados não pode ser menor que 7");
 			sum = 7;
 		}
@@ -78,74 +77,72 @@ public class Tabuleiro {
 			}
 			// continuar
 		}
-		if(jogador.getPosition() > 40) {
+		if (jogador.getPosition() > 40) {
 			jogador.setPosition(40);
 		}
 		squares.get(jogador.getPosition()).addPlayer(jogador);
 	}
 
 	public boolean veriFicarVitoria() {
-		if(!squares.get(40).getPlayers().isEmpty()) {
+		if (!squares.get(40).getPlayers().isEmpty()) {
 			System.out.println(squares.get(40).getPlayers().get(0).getColor() + " venceu :)");
 			return true;
 		}
 		return false;
 	}
-	
-	public String toString() {
-	    StringBuilder sb = new StringBuilder();
 
-for(int i = 0; i <= 10; i++) {
-	int length = squares.get(i).getPlayers().size();
-	sb.append("[");
-	for(int j = 0; j < length; j++) {
-		sb.append(squares.get(i).getPlayers().get(j));
-	}
-	for(int j = 0; j < 6 -length; j++) {
-		sb.append(" ");
-	}
-	sb.append("]");
-}
-sb.append("\n");
-sb.append("\n");
-for(int i = 1; i <= 10; i++) {
-	int lenght1 = squares.get(41 - i).getPlayers().size();
-	int lenght2 = squares.get(10 + i).getPlayers().size();
-	for(int j = 0; j < 6; j++) {
-		for(int k = 0; k < 8; k++) {
-			sb.append(" ");
-		}
-		if(j < lenght1) {
-			sb.append("|").append(squares.get(41 - i).getPlayers().get(j)).append("|");
-		} else {
-			sb.append("|").append(" ").append("|");
-		}
-			for(int k = 0; k < 75; k++) {
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i <= 10; i++) {
+			for(int j = 0; j < 3; j++) {
 				sb.append(" ");
 			}
-			if(j < lenght2) {
-				sb.append("|").append(squares.get(10 + i).getPlayers().get(j)).append("|");
-			} else {
-				sb.append("|").append(" ").append("|");
+			sb.append(i);
+			for(int j = 0; j < 4; j++) {
+				sb.append(" ");
 			}
-			sb.append("\n");
-	}
-	sb.append("\n");
-}
-for(int k = 0; k < 8; k++) {
-	sb.append(" ");
-}
-	for(int i = 1; i <= 10; i++) {
-		int length = squares.get(31 - i).getPlayers().size();
-		sb.append("[");
-		for(int j = 0; j < length; j++) {
-			sb.append(squares.get(31 - i).getPlayers().get(j));
 		}
-		for(int j = 0; j < 6 -length; j++) {
+		sb.append("\n");
+		
+		for (int i = 0; i <= 10; i++) {
+			sb.append(squares.get(i));
+		}
+		sb.append("\n");
+		
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 0; j < 6; j++) {
+				sb.append(" ");
+			}
+			sb.append(41 - i);
+			sb.append(squares.get(41 - i));
+			for (int j = 0; j < 64; j++) {
+				sb.append(" ");
+			}
+			sb.append(squares.get(10 + i));
+			sb.append(10 + i);
+			sb.append("\n");
+		}
+		
+		for (int i = 0; i < 8; i++) {
 			sb.append(" ");
 		}
-		sb.append("]");
-	}
-	    return sb.toString();
+		for (int i = 30; i >= 21; i--) {
+			sb.append(squares.get(i));
+		}
+		sb.append("\n");		
+
+		for(int i = 0; i < 8; i++) {
+			sb.append(" ");
+		}
+		for(int i = 30; i >= 21; i--) {
+			for(int j = 0; j < 3; j++) {
+				sb.append(" ");
+			}
+			sb.append(i);
+			for(int j = 0; j < 3; j++) {
+				sb.append(" ");
+			}
+		}
+		return sb.toString();
 	}
 }
