7580609352306fedff41d3080a1838f0d2b9f93b
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-08-20 17:32:10 -0300

    calcular media

--- TrabalhoPOO3/src/arquivo/Aluno.java
+++ TrabalhoPOO3/src/arquivo/Aluno.java
@@ -5,5 +5,23 @@ public class Aluno {
 	private String nome;
 	private String respostas;
 	private int numAcertos;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRespostas() {
+		return respostas;
+	}
+	public void setRespostas(String respostas) {
+		this.respostas = respostas;
+	}
+	public int getNumAcertos() {
+		return numAcertos;
+	}
+	public void setNumAcertos(int numAcertos) {
+		this.numAcertos = numAcertos;
+	}
 	
 }
