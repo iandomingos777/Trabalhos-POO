f2970540c13342e0c4829a41135d11652edab0db
Vitorfol <vitorfol2302@gmail.com>
2024-08-20 16:34:46 -0300

    Classes atualizadas

--- TrabalhoPOO3/src/arquivo/Disciplina.java
+++ TrabalhoPOO3/src/arquivo/Disciplina.java
@@ -1,5 +1,12 @@
 package arquivo;
 
+import java.util.ArrayList;
+
 public class Disciplina {
 	
+	private String nome;
+	private int numAlunos;
+	private double media;
+	private String caminhoGabaritoOficial;
+	private ArrayList<String> 
 }
