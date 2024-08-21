37c4c6761274117bce091fc6df06c06e90e1f452
iandomingos777 <157146597+iandomingos777@users.noreply.github.com>
2024-08-21 20:00:05 -0300

    correçao criaçao de arquivos

--- TrabalhoPOO3/src/arquivo/Correcao.java
+++ TrabalhoPOO3/src/arquivo/Correcao.java
@@ -1,18 +1,36 @@
 package arquivo;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Correcao {
 
 	public void comecar() {
-		Disciplina d1 = new Disciplina("POO");
-		try {
-			d1.cadastrarGabaritoOficial();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		int opc = 0;
+		Scanner sc = new Scanner(System.in);
+		while (opc != 3) {
+			System.out.println("1 - Criar disciplina 2 - Gerar resultados 3 - Sair");
+			opc = sc.nextInt();
+			try {
+			switch (opc) {
+			case 1:
+				System.out.println("Informe o nome da disciplina");
+				String nome = sc.nextLine();
+				Disciplina d = new Disciplina(nome);
+				d.cadastrarGabaritoOficial();
+				d.registrarClasse();
+				break;
+			case 2:
+				System.out.println("Informe o nome da disciplina");
+				String name =sc.nextLine();
+				Disciplina dis = new Disciplina(name);
+				System.out.println("Informe o endereço do gabarito");
+              break;
+			}
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
 		}
-		d1.registrarClasse();
+		sc.close();
 	}
-	
 }
