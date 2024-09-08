65cbe4bcd975db484a5f9c4c4a2105ee064fd433
Vitorfol <vitorfol2302@gmail.com>
2024-09-08 11:50:41 -0300

    CasaTroca corrigida

--- TrabalhoPOO4/src/jogo2/modelo/casa/CasaTroca.java
+++ TrabalhoPOO4/src/jogo2/modelo/casa/CasaTroca.java
@@ -13,45 +13,56 @@ public class CasaTroca extends Casa {
 		super(posiçao);
 	}
 
-	@Override
-	public void aplicarRegra(Jogador jogador) {
-		System.out.println("Deseja trocar suas moedas por:");
-		System.out.println("1 - Boné 2 - Moletom 3 - Óculos 4 - Não trocar");
-		int opc;
-		opc = Jogo.scanner.nextInt();
-		Tabuleiro.getCasas().get(posiçao).remJogador(jogador);
-		switch (opc) {
-		case 1:
-			if (!(jogador instanceof JogadorComBoné) && jogador.decrementarNumMoedas(1)) {
-				System.out.println(jogador + "comprou um boné");
-				jogador = new JogadorComBoné(jogador);
-			} else {
-				System.out.println("Operação não pode ser realizada");
-			}
-			break;
-		case 2:
-			if (jogador instanceof JogadorComBoné && !(jogador instanceof JogadorComMoletom)
-					&& jogador.decrementarNumMoedas(4)) {
-				System.out.println(jogador + "comprou um moletom");
-				jogador = new JogadorComMoletom(jogador);
-			} else {
-				System.out.println("Operação não pode ser realizada");
-			}
-			break;
-		case 3:
-			if (jogador instanceof JogadorComMoletom && !(jogador instanceof JogadorComOculos)
-					&& jogador.decrementarNumMoedas(7)) {
-				System.out.println(jogador + "comprou um óculos");
-				jogador = new JogadorComOculos(jogador);
-			} else {
-				System.out.println("Operação não pode ser realizada");
-			}
-			break;
-		case 4:
-			System.out.println("Nenhuma compra foi realizada");
-		};
-		Tabuleiro.getCasas().get(posiçao).addJogador(jogador);
-	}
+    @Override
+    public void aplicarRegra(Jogador jogador) {
+        System.out.println("Deseja trocar suas moedas por:");
+        System.out.println("1 - Boné 2 - Moletom 3 - Óculos 4 - Não trocar");
+        int opc;
+        opc = Jogo.scanner.nextInt();
+        Tabuleiro.getCasas().get(posiçao).remJogador(jogador);
+
+        Jogador jogadorDecorado = jogador;
+
+        switch (opc) {
+            case 1:
+                if (!(jogador instanceof JogadorComBoné) && jogador.decrementarNumMoedas(1)) {
+                    System.out.println(jogador + " comprou um boné");
+                    jogadorDecorado = new JogadorComBoné(jogador);
+                } else {
+                    System.out.println("Operação não pode ser realizada");
+                }
+                break;
+            case 2:
+                if (jogador instanceof JogadorComBoné && !(jogador instanceof JogadorComMoletom)
+                        && jogador.decrementarNumMoedas(4)) {
+                    System.out.println(jogador + " comprou um moletom");
+                    jogadorDecorado = new JogadorComMoletom(jogador);
+                } else {
+                    System.out.println("Operação não pode ser realizada");
+                }
+                break;
+            case 3:
+                if (jogador instanceof JogadorComMoletom && !(jogador instanceof JogadorComOculos)
+                        && jogador.decrementarNumMoedas(7)) {
+                    System.out.println(jogador + " comprou um óculos");
+                    jogadorDecorado = new JogadorComOculos(jogador);
+                } else {
+                    System.out.println("Operação não pode ser realizada");
+                }
+                break;
+            case 4:
+                System.out.println("Nenhuma compra foi realizada");
+        }
+        
+        jogadorDecorado.receberAtributos(jogador, jogadorDecorado);
+
+        int index = Tabuleiro.getJogadores().indexOf(jogador);
+        if (index != -1) {
+            Tabuleiro.getJogadores().set(index, jogadorDecorado);
+        }
+
+        Tabuleiro.getCasas().get(posiçao).addJogador(jogadorDecorado);
+    }
 	
 
 }
