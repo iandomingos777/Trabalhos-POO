2e1e49b0a01f24fc04d7cfad60d0e99e107c6fe0
Vitorfol <vitorfol2302@gmail.com>
2024-09-15 22:02:36 -0300

    Update CasaSurpresa.java

--- TrabalhoPOO4/src/jogo2/modelo/casa/CasaSurpresa.java
+++ TrabalhoPOO4/src/jogo2/modelo/casa/CasaSurpresa.java
@@ -7,86 +7,83 @@ import jogo2.modelo.decorator.JogadorComBoné;
 import jogo2.modelo.decorator.JogadorComMoletom;
 import jogo2.modelo.decorator.JogadorComOculos;
 import jogo2.modelo.decorator.JogadorDecorator;
+import jogo2.modelo.factory.JogadorFactory;
 import jogo2.modelo.jogador.Jogador;
-import jogo2.modelo.jogador.JogadorAzarado;
 import jogo2.modelo.jogador.JogadorNormal;
 import jogo2.modelo.jogador.JogadorSortudo;
 
 public class CasaSurpresa extends Casa {
 
-	String strColor;
+    String strColor;
 
-	public CasaSurpresa(int posiçao) {
-		super(posiçao);
-		strColor = "\u001B[36m"; // ciano
-	}
+    public CasaSurpresa(int posiçao) {
+        super(posiçao);
+        strColor = "\u001B[36m"; // ciano
+    }
 
-	@Override
-	public void aplicarRegra(Jogador jogador) {
-		Tabuleiro.getCasas().get(posiçao).remJogador(jogador);
-		int id = jogador.getId();
-		Random random = new Random();
-		int temp = random.nextInt(2) + 1;
+    @Override
+    public void aplicarRegra(Jogador jogador) {
+        Tabuleiro.getCasas().get(posiçao).remJogador(jogador);
+        int id = jogador.getId();
+        Random random = new Random();
+        int temp = random.nextInt(2) + 1;
 
-		Jogador novoJogador = null;
+        Jogador novoJogador = null;
 
-		if (jogador instanceof JogadorSortudo) {
-			switch (temp) {
-			case 1:
-				novoJogador = JogadorFactory.criarJogador(1, id);
-				System.out.println(jogador.getColor() + " virou AZARADO!");
-				break;
-			case 2:
-				novoJogador = JogadorFactory.criarJogador(2, id);
-				System.out.println(jogador.getColor() + " virou NORMAL!");
-				break;
-			}
-		} else if (jogador instanceof JogadorNormal) {
-			switch (temp) {
-			case 1:
-				novoJogador = JogadorFactory.criarJogador(1, id);
-				System.out.println(jogador.getColor() + " virou AZARADO!");
-				break;
-			case 2:
-				novoJogador = JogadorFactory.criarJogador(3, id);
-				System.out.println(jogador.getColor() + " virou SORTUDO!");
-				break;
-			}
-		} else {
-			switch (temp) {
-			case 1:
-				novoJogador = JogadorFactory.criarJogador(2, id);
-				System.out.println(jogador.getColor() + " virou NORMAL!");
-				break;
-			case 2:
-				novoJogador = JogadorFactory.criarJogador(3, id);
-				System.out.println(jogador.getColor() + " virou SORTUDO!");
-				break;
-			}
-		
-		if(jogador instanceof JogadorDecorator) {
-			if(jogador instanceof JogadorComBoné) {
-				novoJogador = new JogadorComBoné(novoJogador);
-			}
-			else if(jogador instanceof JogadorComMoletom) {
-				novoJogador = new JogadorComMoletom(novoJogador);
-			}
-			else if(jogador instanceof JogadorComOculos) {
-				novoJogador = new JogadorComOculos(novoJogador);
-			}
-		}
-		
-		if (novoJogador != null) {
-			novoJogador.receberAtributos(jogador, novoJogador);
-			jogador = novoJogador;
-			Tabuleiro.getCasas().get(posiçao).addJogador(jogador);
-			Tabuleiro.getJogadores().set(jogador.getId() - 1, jogador);
-		}
+        if (jogador instanceof JogadorSortudo) {
+            switch (temp) {
+                case 1:
+                    novoJogador = JogadorFactory.criarJogador(1, id);
+                    System.out.println(jogador.getColor() + " virou AZARADO!");
+                    break;
+                case 2:
+                    novoJogador = JogadorFactory.criarJogador(2, id);
+                    System.out.println(jogador.getColor() + " virou NORMAL!");
+                    break;
+            }
+        } else if (jogador instanceof JogadorNormal) {
+            switch (temp) {
+                case 1:
+                    novoJogador = JogadorFactory.criarJogador(1, id);
+                    System.out.println(jogador.getColor() + " virou AZARADO!");
+                    break;
+                case 2:
+                    novoJogador = JogadorFactory.criarJogador(3, id);
+                    System.out.println(jogador.getColor() + " virou SORTUDO!");
+                    break;
+            }
+        } else {
+            switch (temp) {
+                case 1:
+                    novoJogador = JogadorFactory.criarJogador(2, id);
+                    System.out.println(jogador.getColor() + " virou NORMAL!");
+                    break;
+                case 2:
+                    novoJogador = JogadorFactory.criarJogador(3, id);
+                    System.out.println(jogador.getColor() + " virou SORTUDO!");
+                    break;
+            }
+        }
 
-	}
+        if (jogador instanceof JogadorDecorator) {
+            if (jogador instanceof JogadorComBoné) {
+                novoJogador = new JogadorComBoné(novoJogador);
+            } else if (jogador instanceof JogadorComMoletom) {
+                novoJogador = new JogadorComMoletom(novoJogador);
+            } else if (jogador instanceof JogadorComOculos) {
+                novoJogador = new JogadorComOculos(novoJogador);
+            }
+        }
 
-	public String toString() {
-		return super.toString(strColor);
-	}
+        if (novoJogador != null) {
+            novoJogador.receberAtributos(jogador, novoJogador);
+            jogador = novoJogador;
+            Tabuleiro.getCasas().get(posiçao).addJogador(jogador);
+            Tabuleiro.getJogadores().set(jogador.getId() - 1, jogador);
+        }
+    }
 
+    public String toString() {
+        return super.toString(strColor);
+    }
 }
